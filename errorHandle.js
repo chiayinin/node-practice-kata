a7e67f10c4fa0221704bff7c9463629503a83ed3
chiayinin <chiayinin@gmail.com>
2024-04-08 20:44:13 +0800

    feat: git init

---
+++ errorHandle.js
@@ -0,0 +1,16 @@
+const headers = {
+  "Access-Control-Allow-Headers": "Content-Type, Authorization, Content-Length, X-Reuested-With",
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Methods": "PATCH, POST, GET, OPTIONS, DELETE",
+  "Content-Type": "application/json",
+};
+const errorMsg = {
+  "status": "false",
+  "message": "欄位未填寫正確，或無此 todo id。"
+}
+
+export function errorHandle(res) {
+  res.writeHead(400, headers);
+  res.write(JSON.stringify(errorMsg));
+  res.end();
+};
---
+++ index.js
@@ -0,0 +1,36 @@
+import { v4 as uuidv4 } from 'uuid';
+import http from 'http';
+import { errorHandle } from './errorHandle.js';
+
+const headers = {
+  "Access-Control-Allow-Headers": "Content-Type, Authorization, Content-Length, X-Reuested-With",
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Methods": "PATCH, POST, GET, OPTIONS, DELETE",
+  "Content-Type": "application/json",
+}
+let todos = [
+  {
+    title: "刷刷牙",
+    id: uuidv4(),
+  }
+]
+const successMsg = {
+  "status": "success",
+  "data": todos,
+}
+
+const requestListenner = (request, response) => {
+  if(request.url === '/todos' && request.method === 'GET') {
+    try {
+      response.writeHead(200, headers);
+      response.write(JSON.stringify(successMsg));
+      response.end();
+    } catch(error) {
+      errorHandle(res);
+    }
+  }
+};
+
+const server = http.createServer(requestListenner);
+
+server.listen(3005);
